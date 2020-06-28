4ec32558e0261bdd1813c22648acf5f8705ab4d7
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-06-28 18:09:10 -0500

    [update]: class console in loop with quit & help

--- console.py
+++ console.py
@@ -4,8 +4,37 @@
         HBNBCommand: HbBN Console
 """
 
-from cmd import Cmd
+import cmd
+import sys
 
 
-class HBNBCommand(Cmd):
-    pass
+class HBNBCommand(cmd.Cmd):
+    """ Command interpreter for HbNB clone
+    """
+
+    def __init__(self):
+        """ class constructor
+        """
+
+        cmd.Cmd.__init__(self)
+        self.intro = r"""
+        HbNB Clone command interpreter
+        -----------------------------
+                Welcome Human """
+        self.prompt = '(HBNB) '
+
+    def do_quit(self, arg):
+        """exit from HBNB console
+        """
+
+        sys.exit()
+    
+    def do_EOF(self, arg):
+        """EOF command for forcing exit
+        """
+
+        return True
+
+
+HBNBConsole = HBNBCommand()
+HBNBConsole.cmdloop()
